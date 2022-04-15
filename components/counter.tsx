f804b26bdd92c8ce0092949c619389dbdddf25c9
Tuomas Suutari <tuomas@nepnep.net>
2022-04-15 11:42:42 +0300

    Redo counter with react-bulma-components

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,23 +1,40 @@
-import { useState, useEffect } from "react";
+import {useState} from "react";
+import {Box, Button, Card} from "react-bulma-components";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-
-  function handleAlertClick() {
-    setTimeout(() => {
-      alert("You clicked on: " + count);
-    }, 1000);
-  }
-
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <button onClick={handleAlertClick}>Show alert</button>
-    </div>
+    <Box>
+      <Card style={{width: 180}}>
+        <Card.Header>
+          <Card.Header.Title>Counter</Card.Header.Title>
+        </Card.Header>
+        <Card.Content>Counter value is {count}</Card.Content>
+        <Card.Footer>
+          <Button
+            renderAs={Card.Footer.Item}
+            color="primary"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </Button>
+          <Button
+            renderAs={Card.Footer.Item}
+            color="success"
+            onClick={() => setCount(count - 1)}
+          >
+            -
+          </Button>
+          <Button
+            renderAs={Card.Footer.Item}
+            color="info"
+            onClick={() => alert("Counter is " + count)}
+          >
+            ?
+          </Button>
+        </Card.Footer>
+      </Card>
+    </Box>
   );
 }
